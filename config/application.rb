2268a40009cf71c529e2dde2b92a64ec4066460c
Nick Gibbons <rhg3x@virginia.edu>
2015-06-11 18:07:38 -0400

    changes for heroku compilation

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module DhsiLentil
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0
+    config.assets.version = '1.0'
 
 # get precompilation working on heroku:
 	config.assets.initialize_on_precompile = true
